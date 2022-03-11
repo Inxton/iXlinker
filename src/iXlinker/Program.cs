356f474f84e499a3358822f7845203180df7c715
TomKovac <61820360+TomKovac@users.noreply.github.com>
2022-03-11 18:23:40 +0100

    double backslash in the paths replacement added

--- src/iXlinker/Program.cs
+++ src/iXlinker/Program.cs
@@ -13,6 +13,16 @@ namespace iXlinker
                    .WithParsed(o =>
                    {
                        var linker = new ScanTcProjFile();
+
+                       o.TsProjectFile = o.TsProjectFile.Replace("\\\\", "\\");
+                       o.ActiveTargetPlatform = o.ActiveTargetPlatform.Replace("\\\\", "\\");
+                       o.PlcProjectFile = o.PlcProjectFile.Replace("\\\\", "\\");
+                       o.GenerateMappings = o.GenerateMappings.ToLower();
+                       if (o.GenerateMappings.Contains("true"))
+                       {
+                           o.GenerateMappings = "yes";
+                       }
+                       o.DevenvPath = o.DevenvPath.Replace("\\\\", "\\");
                        linker.RuniXlinker(o.TsProjectFile, o.ActiveTargetPlatform, o.PlcProjectFile, o.GenerateMappings == "yes" ? true : false, o.DevenvPath);                       
                    })
                    .WithNotParsed(o => 
@@ -25,19 +35,35 @@ namespace iXlinker
     class CommandLineOptions
     {
         [Option('t', "tsproj", Required = true, HelpText = "TwinCAT project file [.tsproj].")]
-        public string TsProjectFile { get; set; }
-
-        [Option('c', "plcproj", Required = true, HelpText = "Plc project file [.plcproj].")]
-        public string PlcProjectFile { get; set; }
+        public string TsProjectFile { get { return this.tsProjectFile; } set { if (!string.IsNullOrEmpty(value)){ this.tsProjectFile = ReplaceDoubleBackslash(value); } } }
 
         [Option('p', "platform", Default = "Release|TwinCAT RT (x64)", HelpText = "Active target platform")]
         public string ActiveTargetPlatform { get; set; }
 
+        [Option('c', "plcproj", Required = true, HelpText = "Plc project file [.plcproj].")]
+        public string PlcProjectFile { get { return this.plcProjectFile; } set { if (!string.IsNullOrEmpty(value)) { this.plcProjectFile = ReplaceDoubleBackslash(value); } } }
+
         [Option('g', "generate", Default = "yes", HelpText = "Generate mappings")]
-        public string GenerateMappings { get; set; }
+        public string GenerateMappings { get { return this.generateMappings; } set { if (!string.IsNullOrEmpty(value)) { this.generateMappings = TrueOrYes(value); } } }
 
         [Option('d', "devenv", Default = @"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.com", HelpText = "Path to devenv.com")]
-        public string DevenvPath { get; set; }
+        public string DevenvPath { get { return this.devenvPath; } set { if (!string.IsNullOrEmpty(value)) { this.devenvPath = ReplaceDoubleBackslash(value); } } }
+
+        private string tsProjectFile;
+        private string plcProjectFile;
+        private string generateMappings;
+        private string devenvPath;
+        private string ReplaceDoubleBackslash(string path)
+        {
+            return path.Replace("\\\\", "\\");
+        }
+        private string TrueOrYes(string value)
+        {
+            string retval = "false";
+            if (value.ToLower().Contains("yes") | value.ToLower().Contains("true")) { retval = "yes";}
+            return retval;
+        }
+
+
     }
-    
 }
