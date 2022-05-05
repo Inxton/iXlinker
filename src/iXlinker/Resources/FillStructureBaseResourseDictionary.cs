364ceb07ab5e8fcd1302e8d7986c5758a9d10496
TomKovac <61820360+TomKovac@users.noreply.github.com>
2022-05-05 16:38:59 +0200

    Base structures for InfoData and WcState removed as their visual representation already added into the TcoIo

--- src/iXlinker/Resources/FillStructureBaseResourseDictionary.cs
+++ src/iXlinker/Resources/FillStructureBaseResourseDictionary.cs
@@ -20,9 +20,9 @@ namespace TsprojFile.Scan
             StructureBasesResourceDictionary.Add(StructureBase.Build("*", box, "EtcSlaveTerminalBase", PlcStructuresInPlcLibraries).AddAttribute("{attribute clr[Container(Layout.Stack)]}").AddAttribute("{attribute clr[Group(Layout.GroupBox)]}").AddAttribute("{attribute addProperty PreviousPort \"Unknown\"}"));
             StructureBasesResourceDictionary.Add(StructureBase.Build("Channel_830843C1", pdo, "DigitalInputBase", PlcStructuresInPlcLibraries));
             StructureBasesResourceDictionary.Add(StructureBase.Build("Channel_42CA91F7", pdo, "DigitalOutputBase", PlcStructuresInPlcLibraries));
-            StructureBasesResourceDictionary.Add(StructureBase.Build("WcState_9091E0EB", pdo, "WcStateInputToggleBase", PlcStructuresInPlcLibraries));
-            StructureBasesResourceDictionary.Add(StructureBase.Build("InfoData_32EB74EA", pdo, "InfoDataAdsAddrStateBase", PlcStructuresInPlcLibraries));
-            StructureBasesResourceDictionary.Add(StructureBase.Build("WcState_0B2B16F9", pdo, "WcStateBase", PlcStructuresInPlcLibraries));
+            //StructureBasesResourceDictionary.Add(StructureBase.Build("WcState_9091E0EB", pdo, "WcStateInputToggleBase", PlcStructuresInPlcLibraries));
+            //StructureBasesResourceDictionary.Add(StructureBase.Build("InfoData_32EB74EA", pdo, "InfoDataAdsAddrStateBase", PlcStructuresInPlcLibraries));
+            //StructureBasesResourceDictionary.Add(StructureBase.Build("WcState_0B2B16F9", pdo, "WcStateBase", PlcStructuresInPlcLibraries));
 
 
         }
