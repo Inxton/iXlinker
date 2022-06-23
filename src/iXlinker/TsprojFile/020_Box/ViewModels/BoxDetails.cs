c9baf650fd162877186d442d1c7b120081f0d972
TomKovac <61820360+TomKovac@users.noreply.github.com>
2022-06-23 19:41:18 +0200

    Export of all devices and boxes found in the solution added (#18)
    * EtherCAT master base structure added, plus terminals without PDOs
    
    * Event logger added into the 'GetPlcLibraries' to discover Plc ~library issues
    
    * Discovering IO devices using independent project file added
    
    * TcSmProjectProjectIODevice => TcSmDevDef
    
    * Independent project file support for the IO devices, very first attempt.
    
    * Independent project file support added for the IO devices
    
    * issue with dot in folder name solved
    
    * Disabled devices listed in the log file
    
    * Manually disabled devices and boxes listed in the log file
    
    * Export of all devices and boxes found in the solution added

---
+++ src/iXlinker/TsprojFile/020_Box/ViewModels/BoxDetails.cs
@@ -0,0 +1,85 @@
+﻿namespace iXlinkerDtos
+{
+    public class BoxDetails : NotifiableBase
+    {
+        private string name;
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    this.name = value;
+                    NotifyPropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+        private string ioTreePath;
+        public string IoTreePath
+        {
+            get { return this.ioTreePath; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    this.ioTreePath = value;
+                    NotifyPropertyChanged(nameof(IoTreePath));
+                }
+            }
+        }
+
+        private bool isIndependentProjectFile;
+
+        public bool IsIndependentProjectFile
+        {
+            get { return isIndependentProjectFile; }
+            set
+            {
+                isIndependentProjectFile = value;
+                NotifyPropertyChanged(nameof(IsIndependentProjectFile));
+            }
+        }
+
+
+        private string fileName;
+        public string FileName
+        {
+            get { return this.fileName; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    this.fileName = value;
+                    NotifyPropertyChanged(nameof(FileName));
+                }
+            }
+        }
+
+        private bool fileExists;
+
+        public bool FileExists
+        {
+            get { return fileExists; }
+            set
+            {
+                fileExists = value;
+                NotifyPropertyChanged(nameof(FileExists));
+            }
+        }
+
+        private bool isDisabled;
+
+        public bool IsDisabled
+        {
+            get { return isDisabled; }
+            set
+            {
+                isDisabled = value;
+                NotifyPropertyChanged(nameof(IsDisabled));
+            }
+        }
+
+    }
+}
